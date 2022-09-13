4bfe4442bd633cfd18c02681d49dca2d17877203
Kirill Zhelvakov <blzneci.89@gmail.com>
2022-09-13 18:02:19 +0300

    change case_A.py

--- case_A.py
+++ case_A.py
@@ -5,10 +5,12 @@ import io, os
 input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readlines
 data = input()
 """
-def sum(pairs):
-n = int(data[0])
-pairs = (map(int, line.strip().split()) for line in data[1:])
-print(*(sum(pair for pair in pairs)), sep='\n', end='\n')
-
+tests_path = 'tests_sandbox/tests_A'
+file_list = os.listdir(tests_path)
+tests_list = file_list[::2]
+answer_list = file_list[1::2]
+print('files: ', file_list)
+print('test files are: ', tests_list)
+print('answer files are: ', answer_list)
 
 
